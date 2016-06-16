de9c61da4cc5c7e74b9b98c7d77665f2fcf1cfc0
Paul Gunn <paul.gunn@autodesk.com>
2016-06-16 10:35:38 -0400

    Create API class framework. Implement ident service.

--- vault.js
+++ vault.js
@@ -1,24 +1,57 @@
-var callVaultApi = function(serviceName, methodName, parameters, cb) {
-	var baseUri = 'http://localhost/autodeskdm/Services/';
-    var baseAction = 'http://AutodeskDM/Services/'
-	$.soap({
-		url: baseUri + serviceName + '.svc',
-		SOAPAction: baseAction + serviceName + '/' + methodName,
-		data: parameters || {},
-	
-		success: function (soapResponse) {
-			
-		console.log('success');
-		var response = soapResponse.toJSON();
-		var data = response.Body[methodName + 'Response'][methodName + 'Result'];
-		cb(data);
-		},
-		error: function (SOAPResponse) {
-		console.log('failure')
-		}
-	});
+class API {
+    constructor(options) {
+        this.IdentService = new IdentService();
+    }
 };
 
-var products = callVaultApi('InformationService', 'GetSupportedProducts', {}, function(products) {
-	console.log(products);
-});
+class IdentService {
+    constructor(options) {
+        this._serviceName = "IdentificationService";
+        this._uriPath = 'Filestore/v22/';
+        this._actionPath = 'Filestore/Identification/2/3/2016/';
+    }
+
+    _callAPI(methodName, data, cb) {
+        _soap(this._uriPath, this._actionPath, this._serviceName, methodName, data, cb);
+    };
+
+    GetServerIdentities(cb) {
+        this._callAPI('GetServerIdentities', {}, cb);	
+    }
+};
+
+
+var _soap = function(uriPath, actionPath, serviceName, methodName, parameters, cb) {
+    var baseUri = 'http://localhost/autodeskdm/Services/';
+    var baseAction = 'http://AutodeskDM/'
+    $.soap({
+        url:        baseUri    + uriPath    + serviceName + '.svc',
+        SOAPAction: baseAction + actionPath + serviceName + '/' + methodName,
+        data: parameters || {},
+    
+        success: function (soapResponse) {
+            console.log(methodName + ' success');
+            var response = soapResponse.toJSON();
+            var data = response.Body[methodName + 'Response'][methodName + 'Result']; // unwrap [body, resp, res] to return data
+            cb(null, data);
+        },
+        error: function (soapResponse) {
+            console.log(methodName + ' failure');
+            var response = soapResponse.toJSON();
+            var err = response.Body.Fault; // unwrap [body, fault] to return soap fault
+            cb(err, null);
+       }
+    });
+};
+
+//test
+var api = new API();
+api.IdentService.GetServerIdentities(function(err, idents) {
+    if( err ) {
+        console.log(err.faultcode.text + '|' + err.faultstring.text);
+    } else {
+        console.log(idents); 
+    }
+})
+
+
