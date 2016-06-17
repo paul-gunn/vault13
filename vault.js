3f74acf9fc544f0c20329f0721d9277d4b27cc07
Paul Gunn <paul.gunn@autodesk.com>
2016-06-17 17:01:57 -0400

    Get signin working

--- vault.js
+++ vault.js
@@ -12,10 +12,12 @@ api.IdentService.GetServerIdentities()
 })
 .then(function(vaults) {
    allvaults = vaults;
+   return api.AuthService.SignIn(idents.DataServer, "Administrator", "", allvaults[1].Name);
 })
-.then(function() {
+.then(function(securityHeader) {
     console.log(idents);
     console.log(allvaults);
+    console.log(securityHeader)
 })
 .catch(function(err) {
    console.log(err.faultcode.text + '|' + err.faultstring.text);
--- vaultapi.js
+++ vaultapi.js
@@ -2,6 +2,7 @@ class API {
     constructor(options) {
         this.IdentService = new IdentService();
         this.FilestoreVaultService = new FilestoreVaultService();
+        this.AuthService = new AuthService();
     }
 };
 
@@ -12,11 +13,11 @@ class ServiceBase {
         this._actionPath = actionPath;
     };
 
-    _callAPI(methodName, data) {
+    _callAPI(methodName, data, unwrapResponse = true) {
         data = data || {}
 
         return new Promise(function(resolve, reject) {
-            _soap(this._uriPath, this._actionPath, this._serviceName, methodName, data, function(err, result) {
+            _soap(this._uriPath, this._actionPath, this._serviceName, methodName, data, unwrapResponse, function(err, result) {
                 if( !err ) { 
                     resolve(result);
                 } else { 
@@ -43,7 +44,7 @@ class FilestoreVaultService extends ServiceBase {
         super("FilestoreVaultService", 'Filestore/v22/','Filestore/FilestoreVault/2/3/2016/');
     }
 
-    GetAllKnowledgeVaults(cb) {
+    GetAllKnowledgeVaults() {
         return this._callAPI('GetAllKnowledgeVaults')
         .then(function(vaults) {
             return vaults.KnowledgeVault;  // unwrap  
@@ -51,20 +52,47 @@ class FilestoreVaultService extends ServiceBase {
     }
 };
 
+class AuthService extends ServiceBase {
+    constructor() {
+        super("AuthService", 'Filestore/v22/','Filestore/Auth/2/3/2016/');
+    }
 
-var _soap = function(uriPath, actionPath, serviceName, methodName, parameters, cb) {
+    SignIn(dataServer, userName, userPassword, knowledgeVault) {
+        var data = { dataServer: dataServer, userName: userName, userPassword: userPassword, knowledgeVault: knowledgeVault}
+        return this._callAPI('SignIn', data, false) // don't unwrap response
+        .then(function(soapResponse) {
+            return soapResponse.Header.SecurityHeader;
+      });	
+    }
+};
+
+// <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
+//  <s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+//   <SignIn xmlns="http://AutodeskDM/Filestore/Auth/2/3/2016/">
+//    <dataServer>http://site1/</dataServer><userName>administrator</userName><userPassword/><knowledgeVault>Vault</knowledgeVault></SignIn>
+// </s:Body>
+// </s:Envelope>
+
+
+
+var _soap = function(uriPath, actionPath, serviceName, methodName, parameters, unwrapResponse, cb) {
     var baseUri = 'http://localhost/autodeskdm/Services/';
     var baseAction = 'http://AutodeskDM/'
     $.soap({
         url:        baseUri    + uriPath    + serviceName + '.svc',
         SOAPAction: baseAction + actionPath + serviceName + '/' + methodName,
         data: parameters || {},
-    
+        appendMethodToURL: false,
+        method: methodName,
+        namespaceURL: baseAction + actionPath,
+
         success: function (soapResponse) {
             console.log(methodName + ' success');
             var response = soapResponse.toJSON();
-            var data = response.Body[methodName + 'Response'][methodName + 'Result']; // unwrap [body, resp, res] to return data
-            cb(null, data);
+            if (unwrapResponse) {
+               response = response.Body[methodName + 'Response'][methodName + 'Result']; // unwrap [body, resp, res] to return data
+            }
+            cb(null, response);
         },
         error: function (soapResponse) {
             console.log(methodName + ' failure');
