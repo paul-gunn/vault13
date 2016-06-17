6025e1ff529fff432911303af8c97aa97ace8273
Paul Gunn <paul.gunn@autodesk.com>
2016-06-17 17:35:18 -0400

    Updated test code

--- vault.js
+++ vault.js
@@ -1,23 +1,17 @@
 
 //test
-var api = new API();
+var api = new VaultAPI();
 var idents;
 var allvaults;
 
 
-api.IdentService.GetServerIdentities()
-.then(function(identities) {
-   idents = identities;  
-   return api.FilestoreVaultService.GetAllKnowledgeVaults();
+ api.signIn("Administrator", "", "vault")
+.then(function() {
+    return api.DocService.GetFolderRoot();
 })
-.then(function(vaults) {
-   allvaults = vaults;
-   return api.AuthService.SignIn(idents.DataServer, "Administrator", "", allvaults[1].Name);
-})
-.then(function(securityHeader) {
-    console.log(idents);
-    console.log(allvaults);
-    console.log(securityHeader)
+.then(function(folder) {
+    console.log(folder);
+
 })
 .catch(function(err) {
    console.log(err.faultcode.text + '|' + err.faultstring.text);
